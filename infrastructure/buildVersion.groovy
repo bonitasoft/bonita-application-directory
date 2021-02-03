4e35820cecf6fdaec2b28337f023183fef8e1c16
VishalThapaliya <vishal.thapaliya@gmail.com>
2021-02-03 11:18:05 +0100

    chore (application directory): Create application directory files
    Covers [RUNTIME-110](https://bonitasoft.atlassian.net/browse/RUNTIME-110)

---
+++ infrastructure/buildVersion.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+    options {
+        timestamps()
+    }
+    stages {
+        stage('Build and deploy') {
+            steps {
+                sh "./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
+                withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
+                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./target/bonita-application-directory-${params.version}.bos" 
+                }
+            }
+        }
+    }
+}
---
+++ infrastructure/createTag.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent any
+    options {
+        timestamps()
+        skipDefaultCheckout true
+    }
+    stages {
+        stage('Init') {
+            steps{
+                script{
+                  git branch: params.BASE_BRANCH, url: 'git@github.com:bonitasoft/bonita-application-directory.git'
+                }
+            }
+
+        }
+        stage('Tag') {
+            steps {
+                script {
+                    sh "./infrastructure/release.sh ${params.TAG_NAME} true"
+                    withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
+                        sh "./infrastructure/create-github-release.sh github_api_token=$GITHUB_API_TOKEN tag=${params.TAG_NAME} prerelease=false"
+                    }
+                }
+            }
+        }
+    }
+}
